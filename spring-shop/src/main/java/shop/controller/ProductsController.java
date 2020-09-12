9146c25b9ea7c987ec2d51e5df1fc8312e97a5aa
Alexei <alexei.strelnikov@gmail.com>
2020-09-12 19:40:41 +0300

    HomeTask_Lesson4

--- spring-shop/src/main/java/shop/controller/ProductsController.java
+++ spring-shop/src/main/java/shop/controller/ProductsController.java
@@ -12,6 +12,7 @@ import shop.persist.entity.Product;
 import shop.persist.repo.ProductRepository;
 
 import javax.validation.Valid;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -26,12 +27,65 @@ public class ProductsController {
     private ProductRepository productRepository;
 
     @GetMapping()
-    public String showAllProducts(Model model, @RequestParam(value = "title", required = false) String title){
+    public String showAllProducts(Model model,
+                                  @RequestParam(value = "title", required = false) String title,
+                                  @RequestParam(value = "minprice", required = false) BigDecimal minprice,
+                                  @RequestParam(value = "maxprice", required = false) BigDecimal maxprice,
+                                  @RequestParam(value = "showproduct", required = false) String showproduct){
         logger.info("Filtering by name; {}", title);
-        List<Product> products = productRepository.findAll();
+        List<Product> products;
+
+        if((title == null || title.isEmpty()) & (maxprice == null & minprice == null)){
+            products = productRepository.findAll();
+        }else{
+            if (title == null || title.isEmpty() & (maxprice !=null & minprice != null)){
+               products = productRepository.findByPriceIsGreaterThanEqualAndPriceIsLessThanEqual(minprice, maxprice);
+            }else if (title == null || title.isEmpty() & (maxprice !=null & minprice == null)){
+                products = productRepository.findByPriceIsLessThanEqual(maxprice);
+            }else if (title == null || title.isEmpty() & (maxprice == null & minprice != null)){
+                products = productRepository.findByPriceIsGreaterThanEqual(minprice);
+            }else if (title != null & (maxprice !=null & minprice != null)){
+                products = productRepository.findByTitleLikeAndPriceGreaterThanEqualAndPriceLessThanEqual("%" + title + "%", minprice, maxprice);
+            }else if (title != null & (maxprice == null & minprice != null)){
+                products = productRepository.findByTitleLikeAndPriceIsGreaterThanEqual("%" + title + "%", minprice);
+            }else if (title != null & (maxprice !=null & minprice == null)){
+                products = productRepository.findByTitleLikeAndPriceIsLessThanEqual("%" + title + "%", maxprice);
+            }
+            else {
+                products = productRepository.findByTitleLike("%" + title + "%");
+            }
+        }
+        model.addAttribute("products", products);
+
+        if(showproduct != null) {
+            if (showproduct.equals("Find products with min price")) {
+                products = productRepository.findProductByMinPrice();
+                model.addAttribute("products", products);
+            }else if (showproduct.equals("Find products with max price")) {
+                products = productRepository.findProductByMaxPrice();
+                model.addAttribute("products", products);
+            }else if (showproduct.equals("Find products with min&max price")) {
+                products = productRepository.findProductByMaxAndMinPrice();
+                model.addAttribute("products", products);
+            }
+        }
+        return "products";
+    }
+
+    @GetMapping("/filterbyprice/{price}")
+    public String findeByMinPrice( @PathVariable("price") Integer price, Model model){
+        List<Product> products;
+        if (price == 0) {
+            products = productRepository.findProductByMinPrice();
+        }else if (price == 1){
+            products = productRepository.findProductByMaxPrice();
+        }else {
+            products = productRepository.findProductByMaxAndMinPrice();
+        }
         model.addAttribute("products", products);
         return "products";
     }
+
 
     @GetMapping("/update/{id}")
     public String editProduct(@PathVariable("id") Integer id, Model model){
--- spring-shop/src/main/java/shop/persist/repo/ProductRepository.java
+++ spring-shop/src/main/java/shop/persist/repo/ProductRepository.java
@@ -1,10 +1,39 @@
 package shop.persist.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import shop.persist.entity.Product;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findByTitleLike(String title);
+
+    List<Product> findByPriceIsGreaterThanEqualAndPriceIsLessThanEqual(BigDecimal minprice, BigDecimal maxprice);
+
+    List<Product> findByPriceIsLessThanEqual(BigDecimal maxprice);
+
+    List<Product> findByPriceIsGreaterThanEqual(BigDecimal minprice);
+
+    List<Product> findByTitleLikeAndPriceGreaterThanEqualAndPriceLessThanEqual(String title, BigDecimal minprice, BigDecimal maxprice);
+
+    List<Product> findByTitleLikeAndPriceIsGreaterThanEqual(String title, BigDecimal minprice);
+
+    List<Product> findByTitleLikeAndPriceIsLessThanEqual(String title, BigDecimal maxprice);
+
+
+    @Query("select p from Product p where p.price =(select max (p.price) from Product p)")
+    List<Product> findProductByMaxPrice();
+
+    @Query("select p from Product p where p.price =(select min (p.price) from Product p)")
+    List<Product> findProductByMinPrice();
+
+    @Query("select p from Product p where (p.price =(select max (p.price) from Product p) or p.price =(select min (p.price) from Product p))")
+    List<Product> findProductByMaxAndMinPrice();
+
 
 }
